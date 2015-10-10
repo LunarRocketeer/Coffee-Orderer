5cdb86552fa2bc83705d05ae20c2c45e66eb5b9b
LunarRocketeer <cameron.sidney.miller@gmail.com>
2015-10-10 15:09:54 -0700

    Reinitiliazied...something. This commit includes: Java cleanup Price display Working order button Removal of mail button thing in bottom right corner

--- app/src/main/java/com/example/camer_000/coffeeorderer/MainActivity.java
+++ app/src/main/java/com/example/camer_000/coffeeorderer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.camer_000.coffeeorderer;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,20 +11,33 @@ import java.text.NumberFormat;
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+    private int cups;
+    private int price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cups = 2;
+        price = 5;
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-        displayPrice(2*5);
+        display(cups);
+        displayPrice(cups * price);
+        display(cups);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    public void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     /**
@@ -33,12 +47,5 @@ public class MainActivity extends ActionBarActivity {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-    }
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
